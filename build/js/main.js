cadbcbba9d81c2d9f4e949937da9590c9acaef65
regisperrin <regisperrin@gmail.com>
2024-04-27 09:56:55 +0200

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+let a = 'hello';
+let b = a;
+const addOrConcat = (a, b, c) => {
+    if (c === 'add') {
+        return a + b;
+    }
+    else {
+        return '' + a + b;
+    }
+};
+let myVal = addOrConcat(2, 2, 'contact');
+let myVal1 = addOrConcat(2, 2, 'contact');
+// 
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
+img.src;
+myImg.src;
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+type One = string
+type Two = number | string  
+type Three = 'hello'
+
+
+let a: One = 'hello'
+let b = a as Two
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'contact' ): string | number => {
+    if (c === 'add') {
+        return a + b ;
+    } else {
+        return '' + a + b;
+    }
+};
+
+let myVal: string = addOrConcat(2,2,'contact') as string
+let myVal1: number = addOrConcat(2,2,'contact') as number
+
+
+// 
+const img = document.querySelector('img') as HTMLImageElement
+const myImg = document.getElementById('#img')! as HTMLImageElement
+
+img.src
+myImg.src
+
+
+
+
